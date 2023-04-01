e2c4a64f1d801f44d29fbbe153f311f477e73045
aalonsop <alberto.alonsopinar@gmail.com>
2023-04-01 19:43:27 +0200

    added generation of csv for categories

---
+++ genCSVForBar_cat.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 09:49:42 2023
+
+@author: Jean-Baptiste Filippi - Alberto Alonso-Pinar
+Challenge dataviz 2023 
+
+Processing final du fichier pour en sortir des synthèses graphiques et en CVS pour la bar chart race
+
+
+"""
+
+import numpy as np
+import pandas as pd
+from os import path
+import csv
+
+#dkw = pd.read_pickle("lastRunAllVids.pk")
+#name,value,year,lastValue,rank
+
+import matplotlib.pyplot as plt
+
+# dans cette dataframe, une colonne par mot clé (+1000) + une colonne par nb vue mot clé + une colonne par nb comment mot clé + une colonne ppour la date
+#dkw = pd.read_pickle("lastRunAllVids.pk")
+dkw = pd.read_csv("lastRunAllVids.csv")
+dkw['Date'] =  pd.to_datetime(dkw['DateDay'])
+dkw.set_index('Date', inplace=True)
+
+categories={}
+categories["Film"] = ["comedie","filme","film","cinema","acteurs","tournage"]
+categories["Automobile"] = ["voiture","auto","dakar","electrique"]
+categories["Musique"] = ["musique","skyrock","guitare","guitare"]
+categories["Animaux"] = ["chat","chiennete","animaux"]
+categories["Sports"] = ["mercato","goal","champions","pronostics","musculation","mbappe","sport","sportive","football","barca","psg", "chelsea","madrid"]
+categories["Voyage et evenements"] = ["barcelone","usa","evenement","aventures","aventure","vacances","qatar"]
+categories["Gaming"] = ["jeu","fifa","fortnite" ,"league","streaming", "playstation","games","gameplay","gta","roblox"]
+categories["Divertissement"] = ["cuisine","livre", "camera", "culture", "theatre", "podcast", "comedie", "cinema", "lectures", "magazine"]
+categories["Politique"] = ["guerre","actualite", "international","analyse", "interview", "immigration", "nationale", "analyses" ]
+categories["Beaute"] = ["horoscope","nutrition", "beauty", "astuce", "newsletter", "boutique", "meditation", "gossip'afrique", "coaching" ]
+categories["Education "] = ["education", "tech", "techniques","technique","digital", "energies", "logiciel", "formation", "electrique"]
+categories["Associatif"] = ["jesus","famille", "dimanche", "social", "projet", "membres", "enfants", "priere","chretienne" ]
+categories["Actualites"] = ["ukraine", "covid-19", "macron", "journalistes", "coronavirus", "financier", "president", "russie", "btc","politique" ,"russe"]
+
+
+dcat = pd.DataFrame(index=dkw.index, columns=list(categories.keys()))
+dcat = dcat.fillna(0)
+
+for cat in categories:
+    for catKW in categories[cat]:
+        dcat[cat] = dcat[cat] + ((dkw[catKW]*(1+dkw[catKW+"NV"]))/dkw['NVIDEOS'])
+
+dcat.plot()
+    
+
+
+
+#"coronavirus" "covid-19" 
+#chelsea
+
+kwStart = 0
+kwLimit = 50
+
+dcat['Date'] = dcat.index
+g = dcat.groupby(pd.Grouper(key='Date', freq='36D'))
+dfs = [group for _,group in g]
+
+print("Grouped in %d elements"%len(dfs))
+
+allColumnNames = list(categories.keys())
+allVids = pd.DataFrame(columns=allColumnNames,index=(range(len(dfs))))
+allVids = allVids.fillna(0)
+
+
+for i, dfM in enumerate(dfs):
+    print(i)
+    for index, row in dfM.iterrows():
+        for elt in allColumnNames:
+            allVids.loc[i][elt] += row[elt]
+
+ 
+with open('test.csv',  'w', newline='') as csvfile:
+    mycsvwriter = csv.writer(csvfile, delimiter=',')
+    mycsvwriter.writerow(["name","value","year","lastValue","rank"])
+    
+    for index, row in allVids.iterrows():
+        
+        rankedRow = row.sort_values(ascending=False)
+        
+        for ni, keyRow in enumerate(rankedRow[kwStart:kwStart+kwLimit].keys()):
+            lastValue=rankedRow[keyRow]
+            if(index>0):
+                lastValue=dcat.iloc[index-1][keyRow]
+            mycsvwriter.writerow([keyRow,rankedRow[keyRow],float(2020+(0.1*index)),lastValue,ni+1])
