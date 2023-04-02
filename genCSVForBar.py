714b01e20149175e17e5b83da442d124cac9cab3
FILIPPI Jean-Baptiste <filippi@univ-corse.fr>
2023-04-02 13:44:20 +0200

    nex dataset categories csv compiled

--- genCSVForBar.py
+++ genCSVForBar.py
@@ -28,17 +28,18 @@ dkw['Date'] =  pd.to_datetime(dkw['DateDay'])
 dkw.set_index('Date', inplace=True)
 
 categories={}
+categories["Fortnite"] = ["fortnite"]
 categories["Film"] = ["comedie","filme","film","cinema","acteurs","tournage"]
 categories["Automobile"] = ["voiture","auto","dakar","electrique"]
-categories["Musique"] = ["musique","skyrock","guitare","guitare"]
+categories["Musique"] = ["musique","skyrock","guitare"]
 categories["Animaux"] = ["chat","chiennete","animaux"]
 categories["Sports"] = ["mercato","goal","champions","pronostics","musculation","mbappe","sport","sportive","football","barca","psg", "chelsea","madrid"]
 categories["Voyage et événements"] = ["barcelone","usa","evenement","aventures","aventure","vacances","qatar"]
-categories["Gaming"] = ["jeu","fifa","league","streaming", "playstation","games","gameplay","gta","roblox"] # "fortnite"
+categories["Gaming"] = ["jeu","fifa","league","streaming", "playstation","games","gameplay","gta","roblox","minecraft"] # "fortnite"
 categories["Divertissement"] = ["cuisine","livre", "camera", "culture", "theatre", "podcast", "comedie", "cinema", "lectures", "magazine"]
 categories["Politique"] = ["guerre","actualite", "international","analyse", "interview", "immigration", "nationale", "analyses" ]
 categories["Beauté"] = ["horoscope","nutrition", "beauty", "astuce", "newsletter", "boutique", "meditation", "gossip'afrique", "coaching" ]
-categories["Education "] = ["education", "tech", "techniques","technique","digital", "energies", "logiciel", "formation", "electrique"]
+categories["Education"] = ["education", "tech", "techniques","technique","digital", "energies", "logiciel", "formation", "electrique"]
 categories["Associatif"] = ["jesus","famille", "dimanche", "social", "projet", "membres", "enfants", "priere","chretienne" ]
 categories["Actualites"] = ["ukraine", "covid-19", "macron", "journalistes", "coronavirus", "financier", "president", "russie", "btc","politique" ,"russe"]
 
@@ -53,7 +54,7 @@ for cat in categories:
 
 
 dcat['Date'] = dcat.index
-g = dcat.groupby(pd.Grouper(key='Date', freq='36D'))
+g = dcat.groupby(pd.Grouper(key='Date', freq='2W'))
 dfs = [group for _,group in g]
     
 
@@ -63,17 +64,18 @@ allVids = allVids.fillna(0)
 
 for i, dsub in enumerate(dfs):
     for cat in categories:
+        allVids[cat][i] = dsub[cat].mean()*100
 
-        allVids[cat][i] = dsub[cat].mean()
-                
-        print(i,cat,dsub[cat].mean(),allVids.loc[i][cat])
+allVids = allVids.rolling(2).mean()
+allVids.loc[0] = allVids.loc[1]
 
- 
-with open('test.csv',  'w', newline='') as csvfile:
+outf="categories"
+
+with open(outf+".csv",  'w', newline='') as csvfile:
     mycsvwriter = csv.writer(csvfile, delimiter=',')
     mycsvwriter.writerow(["name","value","year","lastValue","rank"])
     
-    for index, row in allVids.iterrows():
+    for index, row in allVids[1:].iterrows():
         
         rankedRow = row.sort_values(ascending=False)
         
@@ -81,7 +83,15 @@ with open('test.csv',  'w', newline='') as csvfile:
             lastValue=rankedRow[keyRow]
             if(index>0):
                 lastValue=allVids.loc[index-1][keyRow]
-            mycsvwriter.writerow([keyRow,rankedRow[keyRow],float(2020+(0.1*index)),lastValue,ni+1])
+            mycsvwriter.writerow([keyRow,rankedRow[keyRow],index,lastValue,ni+1])
+
+with open(outf+"_p.csv",  'w', newline='') as csvfile:
+    mycsvwriter = csv.writer(csvfile, delimiter=',')
+    mycsvwriter.writerow(["name","year","frequency"])
     
- 
+    for index, row in allVids[1:].iterrows():
+        for k in row.keys():
+            mycsvwriter.writerow([k,index,int(row[k]*100)])
+    
+
 allVids.plot()
