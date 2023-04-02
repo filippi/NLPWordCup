d6dfe2903792442d23f771a2d8cec0514de333ea
FILIPPI Jean-Baptiste <filippi@univ-corse.fr>
2023-04-02 15:21:12 +0200

    final dataset categories csv compiled

--- genCSVForBar.py
+++ genCSVForBar.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Sun Apr  2 13:45:34 2023
+
+@author: filippi_j
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Mar 29 09:49:42 2023
 
 @author: Jean-Baptiste Filippi - Alberto Alonso-Pinar
@@ -22,10 +30,7 @@ dkw = pd.read_pickle("lastRunAllVids.pk")
 import matplotlib.pyplot as plt
 
 # dans cette dataframe, une colonne par mot clé (+1000) + une colonne par nb vue mot clé + une colonne par nb comment mot clé + une colonne ppour la date
-dkw = pd.read_pickle("lastRunAllVids.pk")
 
-dkw['Date'] =  pd.to_datetime(dkw['DateDay'])
-dkw.set_index('Date', inplace=True)
 
 categories={}
 categories["Fortnite"] = ["fortnite"]
@@ -44,6 +49,49 @@ categories["Associatif"] = ["jesus","famille", "dimanche", "social", "projet", "
 categories["Actualites"] = ["ukraine", "covid-19", "macron", "journalistes", "coronavirus", "financier", "president", "russie", "btc","politique" ,"russe"]
 
 
+def genCSVforWeb(fname, listOfKeys, dfs):
+    allVids = pd.DataFrame(columns=listOfKeys,index=(range(len(dfs))))
+    allVids = allVids.fillna(0)
+    
+    for i, dsub in enumerate(dfs):
+        for cat in listOfKeys:
+            allVids[cat][i] = dsub[cat].mean()*100
+    
+    allVids = allVids.rolling(2).mean()
+    allVids.loc[0] = allVids.loc[1]
+    
+    
+    
+    with open(fname+".csv",  'w', newline='') as csvfile:
+        mycsvwriter = csv.writer(csvfile, delimiter=',')
+        mycsvwriter.writerow(["name","value","year","lastValue","rank"])
+        
+        for index, row in allVids[1:].iterrows():
+            
+            rankedRow = row.sort_values(ascending=False)
+            
+            for ni, keyRow in enumerate(rankedRow.keys()):
+                lastValue=rankedRow[keyRow]
+                if(index>0):
+                    lastValue=allVids.loc[index-1][keyRow]
+                mycsvwriter.writerow([keyRow,rankedRow[keyRow],index,lastValue,ni+1])
+    
+    with open(fname+"_p.csv",  'w', newline='') as csvfile:
+        mycsvwriter = csv.writer(csvfile, delimiter=',')
+        mycsvwriter.writerow(["name","year","frequency"])
+        
+        for index, row in allVids[1:].iterrows():
+            for k in row.keys():
+                mycsvwriter.writerow([k,index,int(row[k]*100)])
+        
+    
+    allVids.plot()
+
+dkw = pd.read_pickle("lastRunAllVids.pk")
+
+dkw['Date'] =  pd.to_datetime(dkw['DateDay'])
+dkw.set_index('Date', inplace=True)
+
 dcat = pd.DataFrame(index=dkw.index, columns=list(categories.keys()))
 dcat = dcat.fillna(0)
 
@@ -51,47 +99,30 @@ for cat in categories:
     for catKW in categories[cat]:
         dcat[cat] = dcat[cat] + (dkw[catKW]*(1))/dkw['NVIDEOS']
 
-
-
 dcat['Date'] = dcat.index
 g = dcat.groupby(pd.Grouper(key='Date', freq='2W'))
-dfs = [group for _,group in g]
+dfs = [group for _,group in g]   
+
+listOfKeys = list(categories.keys())
+fname="categories"
+
+genCSVforWeb(fname, listOfKeys, dfs)
+
+for cat in categories:
+    listOfKeys = categories[cat]
+    dcat = pd.DataFrame(index=dkw.index, columns=listOfKeys)
+    dcat = dcat.fillna(0)
+
+    for catKW in listOfKeys:
+        dcat[catKW] = dcat[catKW] + (dkw[catKW]*(1))/dkw['NVIDEOS']
+
+    dcat['Date'] = dcat.index
+    g = dcat.groupby(pd.Grouper(key='Date', freq='2W'))
+    dfs = [group for _,group in g]   
     
+    fname=cat
+
+    genCSVforWeb(fname, listOfKeys, dfs)
 
 
-allVids = pd.DataFrame(columns=list(categories.keys()),index=(range(len(dfs))))
-allVids = allVids.fillna(0)
 
-for i, dsub in enumerate(dfs):
-    for cat in categories:
-        allVids[cat][i] = dsub[cat].mean()*100
-
-allVids = allVids.rolling(2).mean()
-allVids.loc[0] = allVids.loc[1]
-
-outf="categories"
-
-with open(outf+".csv",  'w', newline='') as csvfile:
-    mycsvwriter = csv.writer(csvfile, delimiter=',')
-    mycsvwriter.writerow(["name","value","year","lastValue","rank"])
-    
-    for index, row in allVids[1:].iterrows():
-        
-        rankedRow = row.sort_values(ascending=False)
-        
-        for ni, keyRow in enumerate(rankedRow.keys()):
-            lastValue=rankedRow[keyRow]
-            if(index>0):
-                lastValue=allVids.loc[index-1][keyRow]
-            mycsvwriter.writerow([keyRow,rankedRow[keyRow],index,lastValue,ni+1])
-
-with open(outf+"_p.csv",  'w', newline='') as csvfile:
-    mycsvwriter = csv.writer(csvfile, delimiter=',')
-    mycsvwriter.writerow(["name","year","frequency"])
-    
-    for index, row in allVids[1:].iterrows():
-        for k in row.keys():
-            mycsvwriter.writerow([k,index,int(row[k]*100)])
-    
-
-allVids.plot()
